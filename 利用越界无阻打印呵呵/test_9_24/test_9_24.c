acbb2f82e6849ecfbb0eddcaa7a1a06c1af4b266
Charles <z18729171584@outlook.com>
2019-09-06 17:04:22 +0800

    1

---
+++ 利用越界无阻打印呵呵/test_9_24/test_9_24.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	int arr[10] = { 0 };
+	for (i = 0; i <= 12; i++)
+	{
+		printf("hehe\n");
+		arr[i] = 0;
+	}
+	system ("pause");
+	return 0;
+}
